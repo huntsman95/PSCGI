6b675a8fee73b5a819edf11c01ec32f7f2dd8a33
huntsman95 <huntsman95@gmail.com>
2022-10-31 09:59:08 -0600

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
-
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.Text;
 
 namespace PSCGI
 {
@@ -29,47 +31,81 @@ namespace PSCGI
                 psargsErr = ex.Message;
             }
 
-            Process cmd = new Process();
-            cmd.StartInfo.FileName = "powershell.exe";
-            cmd.StartInfo.Arguments = "-noprofile -file \"" + args[0] + "\" " + psargs;
-            cmd.StartInfo.RedirectStandardInput = true;
-            cmd.StartInfo.RedirectStandardOutput = true;
-            cmd.StartInfo.RedirectStandardError = true;
-            cmd.StartInfo.CreateNoWindow = true;
-            cmd.StartInfo.UseShellExecute = false;
-            cmd.Start();
+            if (Environment.GetEnvironmentVariable("REQUEST_METHOD") == "POST") //experimental POST data
+            {
+                System.IO.Stream s = Console.OpenStandardInput();
+                System.IO.BinaryReader br = new System.IO.BinaryReader(s);
+                string Length = Environment.GetEnvironmentVariable("CONTENT_LENGTH");
+                int Size = Int32.Parse(Length);
+                byte[] Data = new byte[Size];
+                br.Read(Data, 0, Size);
+                // *** don’t close the reader!
+                Environment.SetEnvironmentVariable("POST_DATA", System.Text.Encoding.Default.GetString(Data, 0, Size));
+            }
+
+            /// EXECUTE SCRIPT SECTION
 
             string OutputBuffer = "";
-            string ErrorBuffer = "";
 
-            if(psargsErr != "")
+            using (PowerShell PowerShellInst = PowerShell.Create())
             {
-                ErrorBuffer += psargsErr + "\n";
+                PowerShellInst.AddScript(System.IO.File.ReadAllText(args[0]));
+                Collection<PSObject> results = PowerShellInst.Invoke();
+
+                // close the runspace
+
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (PSObject obj in results)
+                {
+                    stringBuilder.AppendLine(obj.ToString());
+                }
+
+                OutputBuffer = stringBuilder.ToString();
             }
 
-            while (!cmd.HasExited)
-            {
-                OutputBuffer += cmd.StandardOutput.ReadToEnd();
-                ErrorBuffer += cmd.StandardError.ReadToEnd();
-            }
+            /// END EXEC SCRIPT SECTION
 
-            OutputBuffer = OutputBuffer.Trim('\r', '\n');
+            //Process cmd = new Process();
+            //cmd.StartInfo.FileName = "powershell.exe";
+            //cmd.StartInfo.Arguments = "-noprofile -file \"" + args[0] + "\" " + psargs;
+            //cmd.StartInfo.RedirectStandardInput = true;
+            //cmd.StartInfo.RedirectStandardOutput = true;
+            //cmd.StartInfo.RedirectStandardError = true;
+            //cmd.StartInfo.CreateNoWindow = true;
+            //cmd.StartInfo.UseShellExecute = false;
+            //cmd.Start();
+            //
+            //string OutputBuffer = "";
+            //string ErrorBuffer = "";
+            //
+            //if(psargsErr != "")
+            //{
+            //    ErrorBuffer += psargsErr + "\n";
+            //}
+            //
+            //while (!cmd.HasExited)
+            //{
+            //    OutputBuffer += cmd.StandardOutput.ReadToEnd();
+            //    ErrorBuffer += cmd.StandardError.ReadToEnd();
+            //}
 
-            if (!(Regex.Match(OutputBuffer, "Content-type:.*").Success))
-            {
-                OutputBuffer = defaultHeader + OutputBuffer;
-            }
-
-            if (ErrorBuffer != "")
-            {
-                Console.WriteLine("Content-type: text/plain" + Environment.NewLine);
-                Console.WriteLine("The PowerShell script terminated with the following error:" + Environment.NewLine);
-                Console.Write(ErrorBuffer);
-            }
-            else
-            {
-                Console.Write(OutputBuffer);
-            }
+                //OutputBuffer = OutputBuffer.Trim('\r', '\n');
+                //
+                //if (!(Regex.Match(OutputBuffer, "Content-type:.*").Success))
+                //{
+                //    OutputBuffer = defaultHeader + OutputBuffer;
+                //}
+                //
+                ////if (ErrorBuffer != "")
+                ////{
+                ////    Console.WriteLine("Content-type: text/plain" + Environment.NewLine);
+                ////    Console.WriteLine("The PowerShell script terminated with the following error:" + Environment.NewLine);
+                ////    Console.Write(ErrorBuffer);
+                ////}
+                //else
+                //{
+                    Console.Write(OutputBuffer);
+                //}
         }
     }
 }
